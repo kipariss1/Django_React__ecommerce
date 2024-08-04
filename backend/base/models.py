21f16626b5a4cae7bc6a4089946bdf1523cbc1dd
DV37ASE <kirill.rassudikhin@porsche-engineering.cz>
2024-08-04 19:46:47 +0200

    finished with models

--- backend/base/models.py
+++ backend/base/models.py
@@ -29,18 +29,46 @@ class Review(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self) -> str:
-        return "Review on {self.product}, rewiew #{self._id}"
+        return f"Review on {self.product}, rewiew #{self._id}"
     
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
     paymentMethod = models.CharField(max_length=200, null=True, blank=True)
-    taxPrice = models.DecimalField(max_digits=3, decimal_places=2)
+    taxPrice = models.DecimalField(max_digits=7, decimal_places=2)
     shippingPrice = models.DecimalField(max_digits=3, decimal_places=2) 
-    totalPrice = models.DecimalField(max_digits=3, decimal_places=2)
+    totalPrice = models.DecimalField(max_digits=7, decimal_places=2)
     isPaid = models.BooleanField(default=False)
-    paidAt = models.DateTimeField()
+    paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
     isDelivered = models.BooleanField(default=False)
-    deliveredAt = models.DateTimeField()
+    deliveredAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
     createdAt = models.DateTimeField(auto_now_add=True)
     _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return f"Order #{self._id}"
+    
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    quantity = models.IntegerField(default=0, blank=True, null=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    image = models.CharField(max_length=200, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return f"Order item #{self.id} from order #{self.order._id if self.order else 'None'}"
+    
+class ShippingAddress(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    postalCode = models.CharField(max_length=200, null=True, blank=True)
+    country = models.CharField(max_length=200, null=True, blank=True)
+    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.address)
