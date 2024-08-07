eefeb7936ce1a3b83e5411e1cb121efca4800507
DV37ASE <kirill.rassudikhin@porsche-engineering.cz>
2024-08-07 12:33:09 +0200

    almoust finished with redux

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -30,7 +30,7 @@ export const productDetailsReducers = (state = { product: { reviews: [] } }, act
            return {loading: true, ...state}
 
        case PRODUCT_DETAILS_SUCCESS:
-           return {loading: false, products: action.payload}
+           return {loading: false, product: action.payload}
 
        case PRODUCT_DETAILS_FAIL:
            return {loading: false, error: action.payload}
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,59 +1,67 @@
 import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
-import axios from 'axios'
+import { listProductDetails } from '../actions/productActions'
 import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 function ProductScreen() {
     const { id } = useParams();
-    const [product, setProduct] = useState({});
+
+    const dispatch = useDispatch()
+    const productDetails = useSelector(state => state.productDetails)
+    const { error, loading, product = {reviews: []} } = productDetails
+    
     useEffect(() => {
-        (async function getProduct(id) {
-            const {data} = await axios.get(`/api/product/${id}/`);
-            setProduct(data); 
-        })(id);
-    }, [])
+        dispatch(listProductDetails(id))
+    }, [dispatch])
     return (
         <div>
             <Link to="/" className='btn btn-light my-3'>Go Back</Link>
-            <div className='row'>
-                <div className='col md={6}'>
-                    <img src={product.image} alt={product.name} className="img-fluid"></img>
-                </div>
-                <div className='col md={3}'>
-                    <ul className='list-group list-group-flush'>
-                        <li className='list-group-item'><h3>{product.name}</h3></li>
-                        <li className='list-group-item'><h3>Rating: {product.rating}</h3></li>
-                        <li className='list-group-item'><h3>Price: {product.price}$</h3></li>
-                        <li className='list-group-item'><h3>Description: {product.description}</h3></li>
-                    </ul>
-                </div>
-                <div className='col md={3}'>
-                    <div className='card'>
-                        <ul className='list-group list-group-flush'>
-                            <li className='list-group-item'>
-                                <div className='row'>
-                                    <div className='col'>Price:</div>
-                                    <div className='col'>
-                                        <strong>${product.price}</strong>
-                                    </div>
+            {
+                loading ? <Loader></Loader> :
+                    error ? <Message variant="alert alert-danger">Error: {error}</Message> :
+                        <div className='row'>
+                            <div className='col md={6}'>
+                                <img src={product.image} alt={product.name} className="img-fluid"></img>
+                            </div>
+                            <div className='col md={3}'>
+                                <ul className='list-group list-group-flush'>
+                                    <li className='list-group-item'><h3>{product.name}</h3></li>
+                                    <li className='list-group-item'><h3>Rating: {product.rating}</h3></li>
+                                    <li className='list-group-item'><h3>Price: {product.price}$</h3></li>
+                                    <li className='list-group-item'><h3>Description: {product.description}</h3></li>
+                                </ul>
+                            </div>
+                            <div className='col md={3}'>
+                                <div className='card'>
+                                    <ul className='list-group list-group-flush'>
+                                        <li className='list-group-item'>
+                                            <div className='row'>
+                                                <div className='col'>Price:</div>
+                                                <div className='col'>
+                                                    <strong>${product.price}</strong>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li className='list-group-item'>
+                                            <div className='row'>
+                                                <div className='col'>Status:</div>
+                                                <div className='col'>
+                                                    {product.countInStock > 0 ? 'In stock' : 'Out of it'}
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li className='list-group-item'>
+                                            <button type='button' disabled={product.countInStock === 0} className='btn btn-primary d-grid col-6 mx-auto'>Add to card</button>
+                                        </li>
+                                    </ul>
                                 </div>
-                            </li>
-                            <li className='list-group-item'>
-                                <div className='row'>
-                                    <div className='col'>Status:</div>
-                                    <div className='col'>
-                                        {product.countInStock > 0 ? 'In stock' : 'Out of it'}
-                                    </div>
-                                </div>
-                            </li>
-                            <li className='list-group-item'>
-                                <button type='button' disabled={product.countInStock === 0} className='btn btn-primary d-grid col-6 mx-auto'>Add to card</button>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+                            </div>
+                        </div>
+            }
+        </div>    
     )
 }
 
