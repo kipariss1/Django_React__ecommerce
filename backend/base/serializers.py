e441ad7a6bf9f2248695a525e0eef3b395b8bd01
DV37ASE <kirill.rassudikhin@porsche-engineering.cz>
2024-08-09 11:21:26 +0200

    set up endpoints for authentification

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,10 +1,48 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Product, Order, OrderItem, Review, ShippingAddress, User
+from rest_framework_simplejwt.tokens import RefreshToken 
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'name', 'email', 'username', 'isAdmin']
+
+    def get__id(self, object):
+        return object.id
+
+    def get_name(self, object):
+        name = ''
+        if object.first_name and object.last_name:
+            name = object.first_name + ' ' + object.last_name
+        if name == '':
+            name = object.email
+
+        return name
     
+    def get_isAdmin(self, object):
+        return object.is_staff
+    
+
+class UserSerializerWithToken(UserSerializer):
+
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'name', 'email', 'username', 'isAdmin', 'token']
+
+    def get_token(self, object):
+        token = RefreshToken.for_user(object)
+        return str(token.access_token)
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from typing import Dict, Any
 
 # Create your views here.
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs: Dict[str, Any]) -> Dict[str, str]:
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data
+
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+    
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class=MyTokenObtainPairSerializer
+    
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -23,6 +42,12 @@ def getRoutes(request):
         '/api/products/<update>/<id>',
     ]
     return Response(routes)
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProducts(request):
