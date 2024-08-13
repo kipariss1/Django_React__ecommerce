31030cc67292c02addabf0a7ca1600861958a3cc
DV37ASE <kirill.rassudikhin@porsche-engineering.cz>
2024-08-13 10:52:48 +0200

    cleanup finished

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from base.views import user_views as views
 
 urlpatterns = [
-    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('users/register/', views.registerUser, name='register-user'),
-    path('users/profile/', views.getUserProfile, name='user-profile'),
-    path('users/', views.getUsers, name='get-users'),
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('register/', views.registerUser, name='register-user'),
+    path('profile/', views.getUserProfile, name='user-profile'),
+    path('', views.getUsers, name='get-users'),
 ]
 
