20838a286af8426b56f25d9ef09042b68c832b5a
DV37ASE <kirill.rassudikhin@porsche-engineering.cz>
2024-08-15 18:01:00 +0200

    fix navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -12,27 +12,32 @@ function Header() {
         <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
             <div className="container-fluid">
             <Link className="navbar-brand" to="/">Pro shop</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarColor02">
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarColor02">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
-                    <Link to="/cart"><i className='fas fa-shopping-cart'></i>Cart</Link>
+                      <a class="nav-link"><Link to="/cart"><i className='fas fa-shopping-cart'></i>Cart</Link></a>
                     </li>
                     {userInfo ?
-                      <li className='nav-item dropdown'>
-                        <Link className='nav-link dropdown-toggle'>
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                           {userInfo.name}
-                        </Link>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <a class="dropdown-item" href="#">Another action</a>
+                          <div class="dropdown-divider"></div>
+                          <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
                       </li>
                       : 
                       <li className="nav-item">
-                        <Link to="/login"><i className='fas fa-user'></i>Login</Link>
+                        <a class="nav-link"><Link to="/login"><i className='fas fa-user'></i>Login</Link></a>  
                       </li>
                     }
                 </ul>
-                </div>
+              </div>
             </div>
         </nav>
       </header>
