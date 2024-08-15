58615fecf9478f38413444049554e722e1d639e9
DV37ASE <kirill.rassudikhin@porsche-engineering.cz>
2024-08-15 17:40:57 +0200

    fixed header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,17 @@
+import { useDispatch, useSelector } from 'react-redux'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Header() {
+
+  const userLogin = useSelector(state => state.userLogin)
+  const {userInfo} = userLogin
+
   return (
       <header>
         <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Pro shop</a>
+            <Link className="navbar-brand" to="/">Pro shop</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
@@ -15,9 +20,17 @@ function Header() {
                     <li className="nav-item">
                     <Link to="/cart"><i className='fas fa-shopping-cart'></i>Cart</Link>
                     </li>
-                    <li className="nav-item">
-                    <Link to="/login"><i className='fas fa-user'></i>Login</Link>
-                    </li>
+                    {userInfo ?
+                      <li className='nav-item dropdown'>
+                        <Link className='nav-link dropdown-toggle'>
+                          {userInfo.name}
+                        </Link>
+                      </li>
+                      : 
+                      <li className="nav-item">
+                        <Link to="/login"><i className='fas fa-user'></i>Login</Link>
+                      </li>
+                    }
                 </ul>
                 </div>
             </div>
