2a10219fbc57647be77553ff678c36199b8b9835
DV37ASE <kirill.rassudikhin@porsche-engineering.cz>
2024-08-16 09:16:41 +0200

    before pasting chatgpt

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,6 +7,10 @@ function Header() {
   const userLogin = useSelector(state => state.userLogin)
   const {userInfo} = userLogin
 
+  const logoutHandler = () => {
+    console.log("Logging out")
+  }
+
   return (
       <header>
         <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
@@ -21,14 +25,25 @@ function Header() {
                       <a class="nav-link"><Link to="/cart"><i className='fas fa-shopping-cart'></i>Cart</Link></a>
                     </li>
                     {userInfo ?
-                      <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          {userInfo.name}
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                          <a class="dropdown-item" href="#">Another action</a>
-                          <div class="dropdown-divider"></div>
-                          <a class="dropdown-item" href="#">Something else here</a>
+                        <li className="nav-item">
+                          <div class="container-fluid">
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                              <span class="navbar-toggler-icon"></span>
+                            </button>
+                          <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+                            <ul class="navbar-nav">
+                              <li class="nav-item dropdown">
+                                <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                  Dropdown
+                                </button>
+                                <ul class="dropdown-menu dropdown-menu-dark">
+                                  <li><a class="dropdown-item" href="#">Action</a></li>
+                                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                              </li>
+                            </ul>
+                          </div>
                         </div>
                       </li>
                       : 
